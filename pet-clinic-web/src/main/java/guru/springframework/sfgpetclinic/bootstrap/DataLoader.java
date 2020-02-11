5cdc6a778e9617221fc082beb49f25b188e2fa6e
harishrenukunta <harish_r9@yahoo.co.uk>
2020-02-11 23:48:02 +0000

    - List all owners in index page. Closes #18

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,18 +4,18 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader(){
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService){
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
 
@@ -29,7 +29,7 @@ public class DataLoader implements CommandLineRunner {
         Owner owner2 = new Owner();
         owner2.setFirstname("Anil");
         owner2.setLastname("Ravupudi");
-        owner1.setId(2L);
+        owner2.setId(2L);
 
         ownerService.save(owner1.getId(), owner1);
         ownerService.save(owner2.getId(), owner2);
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,15 +1,22 @@
 package guru.springframework.sfgpetclinic.controller;
 
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    @Autowired
+    private OwnerService ownerService;
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listAllOwners(){
+    public String listAllOwners(final Model model){
+        model.addAttribute("owners", ownerService.findAll() );
         return "/owners/index";
     }
 
