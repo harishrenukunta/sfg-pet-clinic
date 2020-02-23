edb118f0e4e5fa2ceda6336c642f67adfc97afee
harishrenukunta <harish_r9@yahoo.co.uk>
2020-02-23 18:46:09 +0000

    - Created Speciality Entity - Added Specialities relationships to Vet.
    Closes #13

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -2,6 +2,10 @@ package guru.springframework.sfgpetclinic.model;
 
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @NoArgsConstructor
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 }
