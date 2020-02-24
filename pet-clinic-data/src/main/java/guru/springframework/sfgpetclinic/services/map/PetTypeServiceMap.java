d186045c9e3b0e1a0f92bfa0f686d3463f16b03d
harishrenukunta <harish_r9@yahoo.co.uk>
2020-02-24 22:16:46 +0000

    - Modified pet type service map to bootstrap.Closes #32

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetTypeServiceMap extends AbstractCrudService<PetType, Long> implements PetTypeService {
 }
