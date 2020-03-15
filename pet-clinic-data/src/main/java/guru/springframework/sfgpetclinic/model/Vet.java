e89175c64095f9c7da3b9c7294a71ffe499014e0
harishrenukunta <harish_r9@yahoo.co.uk>
2020-03-15 22:39:10 +0000

    - Added service for Vet. closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -11,9 +11,18 @@ import java.util.Set;
 @Data
 @Entity
 @Table(name="vets")
-public class Vet extends Person {
+public class Vet extends Person implements Cloneable{
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="vet_specialities", joinColumns = @JoinColumn(name="vet_id"), inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>() ;
+
+    public Vet clone() throws CloneNotSupportedException {
+        Vet v1 = (Vet)super.clone();
+        v1.setFirstname(this.getFirstname());
+        v1.setLastname(this.getLastname());
+        v1.setId(this.getId());
+        v1.setSpecialities(new HashSet<>(this.getSpecialities()));
+        return v1;
+    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatjpa/VetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatjpa/VetSDJpaService.java
@@ -1,4 +1,46 @@
 package guru.springframework.sfgpetclinic.services.springdatjpa;
 
-public class VetSDJpaService {
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> allVets = new HashSet<>();
+        vetRepository.findAll().forEach( allVets::add);
+        return allVets;
+    }
+
+    @Override
+    public Vet save(Vet entity) {
+        return vetRepository.save(entity);
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet delete(Vet object) {
+        Vet toDeleteVet = null;
+        try {
+            toDeleteVet = object.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        vetRepository.delete(object);
+        return toDeleteVet;
+    }
 }
